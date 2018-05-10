84c0acc3020566b8903adf10ca4d265a24edf996
jasminejeane <jasminejeane@gmail.com>
2018-05-09 22:07:02 -0700

    Turret is functional.

--- src/app/internal-defenses/internal-defenses.module.ts
+++ src/app/internal-defenses/internal-defenses.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DeathSquareComponent } from './death-square/death-square.component';
 import { TurretComponent } from './turret/turret.component';
 import { LaserCannonComponent } from './laser-cannon/laser-cannon.component';
+import { AppRoutingModule } from '../app-routing.module'; // replace RouterModule with this import statement
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule 
   ],
   declarations: [
     DeathSquareComponent,
