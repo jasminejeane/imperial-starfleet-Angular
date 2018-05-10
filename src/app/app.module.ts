1895824380fbb6df571634b0c37272f3ff927686
jasminejeane <jasminejeane@gmail.com>
2018-05-09 21:55:42 -0700

    Import this routing module

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { InternalDefensesModule } from './internal-defenses/internal-defenses.module'
+
+import { AppComponent } from './app.component';
+import { TieFighterComponent } from './tie-fighter/tie-fighter.component';
+import { InfoWindowComponent } from './info-window/info-window.component';
+
+import { AppRoutingModule } from './app-routing.module'; // replace RouterModule with this import statement
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TieFighterComponent,
+    InfoWindowComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InternalDefensesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
